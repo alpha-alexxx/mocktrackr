4790cdb736d02bfc861385253b7d48b76b97d28c
Ankit Kumar <ankithome8@gmail.com>
2025-05-14 16:51:24 +0530

    Revert "refactor(next.config.ts): enhance webpack config and improve comments"
    This reverts commit 798295347cdd39559de338b330afb6ad1dc3d9c0.

--- next.config.ts
+++ next.config.ts
@@ -2,27 +2,17 @@ import type { NextConfig } from 'next';
 
 import initializeBundleAnalyzer from '@next/bundle-analyzer';
 
-// Initialize bundle analyzer with the condition to enable based on environment variable
+// https://www.npmjs.com/package/@next/bundle-analyzer
 const withBundleAnalyzer = initializeBundleAnalyzer({
     enabled: process.env.BUNDLE_ANALYZER_ENABLED === 'true'
 });
 
-// Main Next.js config object
+// https://nextjs.org/docs/pages/api-reference/next-config-js
 const nextConfig: NextConfig = {
-    output: 'standalone', // Produces a standalone build for serverless deployments
+    output: 'standalone',
     experimental: {
-        nodeMiddleware: true // Enables experimental Node.js middleware support
-    },
-    webpack(config) {
-        // Add a new rule to ignore markdown (.md) and LICENSE files
-        config.module.rules.push({
-            test: /\.(md|LICENSE)$/,
-            use: 'ignore-loader'
-        });
-
-        return config;
+        nodeMiddleware: true
     }
 };
 
-// Export the configuration with bundle analyzer applied
 export default withBundleAnalyzer(nextConfig);
