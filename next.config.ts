798295347cdd39559de338b330afb6ad1dc3d9c0
Ankit Kumar <ankithome8@gmail.com>
2025-05-14 16:17:43 +0530

    refactor(next.config.ts): enhance webpack config and improve comments
    Add a new webpack rule to ignore markdown and LICENSE files for better build optimization. Improve code comments for clarity and maintainability.

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,27 @@ import type { NextConfig } from 'next';
 
 import initializeBundleAnalyzer from '@next/bundle-analyzer';
 
-// https://www.npmjs.com/package/@next/bundle-analyzer
+// Initialize bundle analyzer with the condition to enable based on environment variable
 const withBundleAnalyzer = initializeBundleAnalyzer({
     enabled: process.env.BUNDLE_ANALYZER_ENABLED === 'true'
 });
 
-// https://nextjs.org/docs/pages/api-reference/next-config-js
+// Main Next.js config object
 const nextConfig: NextConfig = {
-    output: 'standalone',
+    output: 'standalone', // Produces a standalone build for serverless deployments
     experimental: {
-        nodeMiddleware: true
+        nodeMiddleware: true // Enables experimental Node.js middleware support
+    },
+    webpack(config) {
+        // Add a new rule to ignore markdown (.md) and LICENSE files
+        config.module.rules.push({
+            test: /\.(md|LICENSE)$/,
+            use: 'ignore-loader'
+        });
+
+        return config;
     }
 };
 
+// Export the configuration with bundle analyzer applied
 export default withBundleAnalyzer(nextConfig);
